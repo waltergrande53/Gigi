e92ef4b568170aa156325d92f5ee18ed9858defb
waltergrande53 <59238468+waltergrande53@users.noreply.github.com>
2020-10-06 09:39:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -25,13 +25,14 @@ function submitForm(e) {
   let email = document.querySelector(".email").value;
   let message = document.querySelector(".message").value
   saveContactInfo(name, email, message);
-  document.querySelector(".contact-form").reset();
-   if(name.value=''){
-    alert('name required')
+
+  if (name, email == '') {
+    alert('name and email are required to book your order')
     return;
   }
- 
-  sentEmail(name, email,message)
+
+  document.querySelector(".contact-form").reset();
+  sentEmail(name, email, message)
 }
 
 // Save infos to Firebase
@@ -45,12 +46,14 @@ function saveContactInfo(name, email, message) {
   });
 }
 
-function sentEmail(name, email, message){
+function sentEmail(name, email, message)
+{
+
   Email.send({
     Host: "smtp.gmail.com",
     Username: "waltergrande53@gmail.com",
     Password: "soxopankraodwpas",
-    To: 'Gigishoppp88@gmail.com',
+    To: 'waltergrande53@gmail.com',
     From: `${email}`,
     Subject: `${name} sent you a message`,
     Body: ` Message:${message}`
@@ -63,13 +66,14 @@ function sentEmail(name, email, message){
 
 
 
-
+let size = document.querySelector('.size').options[0].selected
 
 order_data = document.querySelector('#order');
-let items = ['Skittles', 'Sour patch watermelon', 'Gummy bears', 'Peaches','Sour patch kids'];
-let total=0;
-let total_amount= document.querySelector("#total")
-let quantity =0;
+let items = ['Skittles', 'Sour patch watermelon', 'Gummy bears', 'Peaches', 'Sour patch kids'];
+let total = 0;
+let size_display;
+let total_amount = document.querySelector("#total")
+let quantity = 0;
 let home = document.getElementById('home');
 let shop = document.querySelector('.container');
 let cart = document.querySelector('#cart')
@@ -77,76 +81,110 @@ let check_out_btn = document.querySelector('#check_out_btn');
 let div = document.createElement('div');
 
 
-  
 
-function selected_item(i){
-  total+=5
- quantity=1;
- console.log($('#order #total').last())
- check_out_btn.style.display='block'
- let div = document.createElement('div');
-    div.className ='list_items'
-   let delete_btn = document.createElement("button");
-  delete_btn.className='delete_btn'
-   order_data.appendChild(div).innerHTML = `${items[i]} ${quantity} `;
-   total_amount.innerHTML = `total: $${total} `;
-  div.appendChild(delete_btn).innerHTML='remove';
-  
- delete_btn.addEventListener('click', () => {
-   total-=5;
-   order_data.removeChild(div)
-   div.removeChild(delete_btn)
+$(document).ready(function() {
+  $(".size").change(function() {
+    selectedSize = $(this).children("option:selected").val();
+    if (selectedSize == null) {
+      return;
+    }
+    selected_price = parseFloat(selectedSize)
+
+  });
+  return selectedSize = null;
+})
+
+function selected_item(i) {
+  quantity = 1;
+  total += selected_price;
+
+  if (selected_price === 5) {
+    size_display = 'Small'
+  }
+  else if (selected_price === 8) {
+    size_display = 'Medium'
+
+  }
+  else if (selected_price === 10) {
+    size_display = 'big'
+  }
+  check_out_btn.style.display = 'block'
+  let div = document.createElement('div');
+  div.className = 'list_items'
+  let delete_btn = document.createElement("button");
+  delete_btn.className = 'delete_btn'
+  order_data.appendChild(div).innerHTML = `${items[i]} ${size_display} ${quantity} `;
+  total_amount.innerHTML = `total: $${total} `;
+  div.appendChild(delete_btn).innerHTML = 'remove';
+  delete_btn.addEventListener('click', () => {
+    total -= selected_price;
+    order_data.removeChild(div)
+    div.removeChild(delete_btn)
     total_amount.innerHTML = `total: $${total} `;
     if (total === 0) {
-      check_out_btn.style.display='none'
+      check_out_btn.style.display = 'none'
     }
- })
+  })
 };
 
- 
- 
- 
- 
- 
- function home_display() {
-   home.style.display = 'block'
-   shop.style.display = 'none'
-   cart.style.display = 'none'
-    document.querySelector('.contact-form').style.display = 'none'
-  
- }
- 
- function shop_display() {
-   let text = $('#order button').text('remove');
-   shop.style.display = 'block'
-   home.style.display = 'none'
-   cart.style.display = 'none'
-   order_data.style.display='none'
-    order_data.style.display='block'
-     document.querySelector('.contact-form').style.display = 'none'
-  
- }
- 
- function cart_display() {
-   cart.style.display = 'block'
-   order_data.display='none'
-   home.style.display = 'none'
-   shop.style.display = 'none'
-   check_out_btn.style.display='none'
-   document.querySelector('.contact-form').style.display='none'
- }
- function check_out(message) {
-   if (quantity === 0) {
-     alert('empty')
-     return;
-   }
-    order_data.style.display='none'
-       text = $('#order button').text('');
 
-    cart.style.display = 'block'
-    home.style.display = 'none'
-    shop.style.display = 'none'
-    check_out_btn.style.display='none'
-   document.querySelector('.contact-form').style.display = 'block'
-   message = document.querySelector(".message").value =order_data.innerText;
- }
+
+
+
+
+function home_display() {
+  home.style.display = 'block'
+  shop.style.display = 'none'
+  cart.style.display = 'none'
+  document.querySelector('.contact-form').style.display = 'none'
+
+}
+
+function shop_display() {
+  let text = $('#order button').text('remove');
+  shop.style.display = 'block'
+  home.style.display = 'none'
+  cart.style.display = 'none'
+  $('#shop-section').css('display', 'block')
+  document.querySelector('#items-title').style.display = 'block'
+  document.querySelector("#order-title").style.display = 'block'
+  order_data.style.display = 'none'
+  order_data.style.display = 'block'
+  document.querySelector('.contact-form').style.display = 'none'
+
+}
+
+function cart_display() {
+  cart.style.display = 'block'
+  order_data.display = 'none'
+  home.style.display = 'none'
+  shop.style.display = 'none'
+  $('#shop-section').css('display', 'none')
+  $('#items-litle').css('display', 'none')
+  if (total > 0) {
+    check_out_btn.style.display = 'block'
+  } else {
+    check_out_btn.style.display = 'none'
+  }
+  document.querySelector('.contact-form').style.display = 'none'
+}
+
+function check_out(message) {
+  if (quantity === 0) {
+    alert('empty')
+    return;
+  }
+  order_data.style.display = 'none'
+  text = $('#order button').text('');
+  document.querySelector('#order-title').style.display = 'none'
+
+  cart.style.display = 'block'
+  home.style.display = 'none'
+  $('.contact-form-section').css('display', 'flex')
+  shop.style.display = 'none'
+  check_out_btn.style.display = 'none'
+  $('#shop-section').css('display', 'none')
+  $('#items-litle').css('display', 'none')
+  document.querySelector('.contact-form').style.display = 'block'
+  message = document.querySelector(".message").value = order_data.innerText;
+}
